48e7be5a70613970efb6830f68470a0e14442629
Shilpa290720 <work.shilpa29@gmail.com>
2025-09-20 22:34:45 +0530

    Update DataCleaningSQL.sql

--- DataCleaningSQL.sql
+++ DataCleaningSQL.sql
@@ -1,24 +1,16 @@
-
-
-
-
-
-
-
 SELECT * 
 FROM world_layoffs.layoffs;
 
 
+-- created a staging table so that we shouldnt delete the original and that the raw data remains intact
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
 INSERT layoffs_staging 
 SELECT * FROM world_layoffs.layoffs;
 
-
--- now when we are data cleaning we usually follow a few steps
+ -- whenever we are data cleaning we usually follow a few steps
 -- 1. check for duplicates and remove any
 -- 2. standardize data and fix errors
 -- 3. Look at null values and see what 
